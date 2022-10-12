61fce01bfd9327ab0419e05a92dbceb63e1249ab
Taokhan <58807914+Taokhan@users.noreply.github.com>
2022-10-12 18:28:11 +0300

    'master'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { RecipeService } from './recipes/recipe.service';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: 
+  [
+    AppComponent,
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule
+  imports: 
+  [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    AuthModule,
+    SharedModule
   ],
-  providers: [],
+  providers:[ShoppingListService,RecipeService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
